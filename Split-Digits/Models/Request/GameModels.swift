f7e306741b7db88c193eaa5cb34638f5f56a46ed
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-29 00:13:33 +0530

    chore: adding game models data

--- Split-Digits/Models/Request/GameModels.swift
+++ Split-Digits/Models/Request/GameModels.swift
@@ -2,12 +2,12 @@
 //  Grids.swift
 //  Split-Digits
 //
-//  Created by MacBookPro on 21/03/1946 Saka.
+//  Created by Bhavin Pathak on 28/06/2024
 //
 
 import Foundation
 
-struct Grids: Identifiable {
+struct GameModels: Identifiable {
     let id = UUID()
     let value: Int
     var isRevealed: Bool = false
